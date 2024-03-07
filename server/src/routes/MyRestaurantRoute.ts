a2907d4c0757ebccc9162f0b51c13694e1ca68f9
xingr121 <137085577+xingr121@users.noreply.github.com>
2024-03-07 10:06:38 -0500

    server create restaurant

--- server/src/routes/MyRestaurantRoute.ts
+++ server/src/routes/MyRestaurantRoute.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import multer from "multer";
 import MyRestaurantController from "../controllers/MyRestaurantController";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyRestaurantRequest } from "../middleware/validation";
 
 const storage = multer.memoryStorage();
 const upload = multer({
@@ -13,6 +15,9 @@ const router = express.Router();
 router.post(
   "/",
   upload.single("imageFile"),
+  validateMyRestaurantRequest,
+  jwtCheck,
+  jwtParse,
   MyRestaurantController.createMyRestaurant
 );
 export default router;
