df0f3c737cacfcb40fa8609494ce6a0f0750a5c5
xingr121 <137085577+xingr121@users.noreply.github.com>
2024-03-07 16:13:57 -0500

    createresturant

---
+++ client/src/pages/ManageRestaurantPage.tsx
@@ -0,0 +1,5 @@
+import ManageRestaurantForm from "@/forms/user-profile-form/manage-restaurant-form/ManageRestaurantForm";
+
+export default function ManageRestaurantPage() {
+  return <ManageRestaurantForm />;
+}
--- server/src/controllers/MyRestaurantController.ts
+++ server/src/controllers/MyRestaurantController.ts
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 
 const createMyRestaurant = async (req: Request, res: Response) => {
   try {
-    const existingRestaurant = await Restaurant.find({ user: req.userId });
+    const existingRestaurant = await Restaurant.findOne({ user: req.userId });
     if (existingRestaurant) {
       return res
         .status(409)
