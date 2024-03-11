8b6d333e769836a4b84c777ab8758d62510c5454
xingr121 <137085577+xingr121@users.noreply.github.com>
2024-03-11 15:52:46 -0400

    order

--- client/src/api/OrderApi.tsx
+++ client/src/api/OrderApi.tsx
@@ -1,10 +1,10 @@
-//import { Order } from "@/types";
+import { Order } from "@/types";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useMutation, useQuery } from "react-query";
 import { toast } from "sonner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-/*
+
 export const useGetMyOrders = () => {
   const { getAccessTokenSilently } = useAuth0();
 
@@ -34,7 +34,7 @@ export const useGetMyOrders = () => {
 
   return { orders, isLoading };
 };
-*/
+
 type CheckoutSessionRequest = {
   cartItems: {
     menuItemId: string;
--- server/src/routes/MyRestaurantRoute.ts
+++ server/src/routes/MyRestaurantRoute.ts
@@ -13,7 +13,19 @@ const upload = multer({
     fileSize: 5 * 1024 * 1024,
   },
 });
+router.get(
+  "/order",
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.getMyRestaurantOrders
+);
 
+router.patch(
+  "/order/:orderId/status",
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.updateOrderStatus
+);
 router.get("/", jwtCheck, jwtParse, MyRestaurantController.getMyRestaurant);
 
 router.post(
